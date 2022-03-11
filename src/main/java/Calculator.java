14131ff938d60f06a709b8233ecb32d6d323bade
Jacob Taylor <jacob.taylor0299@gmail.com>
2022-03-10 21:21:25 -0700

    Tests

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,4 @@
+import java.util.UUID;
 
 class Calculator {
 
@@ -39,7 +40,23 @@ class Calculator {
     etc
      */
     int fibonacciNumberFinder(int n){
-        return 0;
+        int f[] = new int[n + 1];
+        int i;
+
+        /* 0th and 1st number of the series are 0 and 1*/
+        f[0] = 0;
+
+        if (n > 0) {
+            f[1] = 1;
+
+            for (i = 2; i <= n; i++) {
+                /* Add the previous 2 numbers in the series
+             and store it */
+                f[i] = f[i - 1] + f[i - 2];
+            }
+        }
+
+        return f[n];
     }
 
 
@@ -51,7 +68,7 @@ class Calculator {
     if int a = 16 then this method returns: 10000
      */
     String intToBinaryNumber(int n){
-        return null;
+        return Integer.toBinaryString(n);
     }
 
     /*
@@ -63,8 +80,8 @@ class Calculator {
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
     String createUniqueID(String n){
-
         return null;
+        //return n + UUID.randomUUID();
     }
 
 
